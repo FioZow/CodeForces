510db15bea2397e33cd51de8fd2426b81604cfe9
FioZow <domahin1995@gmail.com>
2022-05-09 00:18:54 +0300

    Test a

---
+++ sandbox/test A/Summator/Summator/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Linq;
+
+namespace Summator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var count = int.Parse(Console.ReadLine());
+
+            for(int i = 0; i < count; i++)
+            {
+                var numbers = Console.ReadLine().Split(" ").Select(it => int.Parse(it)).ToArray();
+                Console.WriteLine(numbers.Sum());
+            }
+        }
+    }
+}
