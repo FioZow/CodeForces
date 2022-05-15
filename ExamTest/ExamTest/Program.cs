905872a8adb6c875cb1e542b894e2cd8ea214e19
FioZow <domahin1995@gmail.com>
2022-05-15 13:32:36 +0300

    new test

---
+++ ExamTest/ExamTest/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Linq;
+
+namespace ExamTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var testsCount = uint.Parse(Console.ReadLine());
+
+            for (int i = 0; i < testsCount; i++)
+            {
+                var productsCount = uint.Parse(Console.ReadLine());
+                var prices = Console.ReadLine().Split(" ").Select(it => uint.Parse(it)).ToList();
+                var group = prices.GroupBy(x => x);
+
+                uint result = 0;
+
+                foreach (var item in group)
+                {
+                    var count = (uint)item.Count();
+                    uint saleProductsCount = 0;
+
+                    while (count >= 3)
+                    {
+                        saleProductsCount++;
+                        count -= 3;
+                    }
+
+                    result += item.Key * ((uint)item.Count() - saleProductsCount);
+                }
+                Console.WriteLine(result);
+            }
+        }
+    }
+}
