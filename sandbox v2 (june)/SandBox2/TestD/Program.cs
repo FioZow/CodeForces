40d60a8e983bd66ecf9bbf3c42282e3991759e59
FioZow <domahin1995@gmail.com>
2022-06-25 18:11:54 +0300

    TestD complete

--- sandbox v2 (june)/SandBox2/TestD/Program.cs
+++ sandbox v2 (june)/SandBox2/TestD/Program.cs
@@ -3,15 +3,52 @@
 while (testsCount != 0)
 {
     var loginsCount = int.Parse(Console.ReadLine());
-
-    var loginsSet = new HashSet<string>(loginsCount);
+    var inputList = new List<string>(loginsCount);
 
     while (loginsCount != 0)
     {
-
-
+        inputList.Add(Console.ReadLine());
         loginsCount--;
     }
 
+    var loginsSet = new HashSet<string>(loginsCount);
+
+    foreach (var item in inputList)
+    {
+        if (Validate(item) && loginsSet.Add(item.ToLower()))
+            Console.WriteLine("YES");
+        else
+            Console.WriteLine("NO");
+    }
+    Console.WriteLine();
     testsCount--;
+}
+
+
+static bool Validate(string login)
+{
+    if (login == null)
+        return false;
+    if (login.Length < 2 || login.Length > 24)
+        return false;
+    if (login.StartsWith("-") || login.Contains(" "))
+        return false;
+
+    var chars = login.ToCharArray();
+
+    foreach (var item in chars)
+    {
+        if (item < 45 || item > 122)
+            return false;
+        if (item > 45 && item < 48)
+            return false;
+        if (item > 57 && item < 65)
+            return false;
+        if (item > 90 && item < 95)
+            return false;
+        if (item > 95 && item < 97)
+            return false;
+    }
+
+    return true;
 }
