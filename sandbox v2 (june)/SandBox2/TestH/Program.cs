0ce43c212fb402d6bb3bdc7a4c57b430cb8ba73a
FioZow <domahin1995@gmail.com>
2022-06-30 13:33:49 +0300

    TestH created

---
+++ sandbox v2 (june)/SandBox2/TestH/Program.cs
@@ -0,0 +1,35 @@
+﻿var testsCount = int.Parse(Console.ReadLine());
+
+while (testsCount != 0)
+{
+    var filedDimensions = Console.ReadLine().Split(" ");
+    var rowsCount = int.Parse(filedDimensions[0]);
+    var columnsCount = int.Parse(filedDimensions[1]);
+
+    var field = new char[rowsCount, columnsCount];
+
+    for (int i = 0; i < rowsCount; i++)
+    {
+        for (int j = 0; j < columnsCount; j++)
+        {
+            field[i, j] = Console.ReadKey().KeyChar;
+        }
+        Console.ReadKey();
+    }
+
+
+
+    testsCount--;
+}
+
+void PrintArray(char[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write(array[i, j]);
+        }
+        Console.WriteLine();
+    }
+}
