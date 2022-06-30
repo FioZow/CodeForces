fbc45c484453d347b80c4556437f651fb86d9a2d
FioZow <domahin1995@gmail.com>
2022-06-30 14:32:34 +0300

    carcass

--- sandbox v2 (june)/SandBox2/TestH/Program.cs
+++ sandbox v2 (june)/SandBox2/TestH/Program.cs
@@ -6,18 +6,37 @@ while (testsCount != 0)
     var rowsCount = int.Parse(filedDimensions[0]);
     var columnsCount = int.Parse(filedDimensions[1]);
 
-    var field = new char[rowsCount, columnsCount];
+    var field = new List<Point>();
 
     for (int i = 0; i < rowsCount; i++)
     {
         for (int j = 0; j < columnsCount; j++)
         {
-            field[i, j] = Console.ReadKey().KeyChar;
+            var ch = Console.ReadKey().KeyChar;
+            var point = new Point(j, i, ch == '*');
+
+            field.Add(point);
         }
         Console.ReadKey();
     }
 
+    var pointsCount = field.Count;
 
+    for (int i = 0; i < pointsCount; i++)
+    {
+        field[i].IsVisited = true;
+
+    }
+
+    foreach (var point in field)
+    {
+        if (point.IsShip && !point.IsVisited)
+        {
+
+        }
+
+        point.IsVisited = true;
+    }
 
     testsCount--;
 }
@@ -31,5 +50,26 @@ void PrintArray(char[,] array)
             Console.Write(array[i, j]);
         }
         Console.WriteLine();
+    }
+}
+
+class Ship
+{
+    public int Size { get; set; }
+    public List<Point> Points { get; set; }
+}
+
+public class Point
+{
+    public int X { get; set; }
+    public int Y { get; set; }
+    public bool IsVisited { get; set; }
+    public bool IsShip { get; set; }
+    public Point(int x, int y, bool isShip)
+    {
+        X = x;
+        Y = y;
+        IsVisited = false;
+        IsShip = isShip;
     }
 }
