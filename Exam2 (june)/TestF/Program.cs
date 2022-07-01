48c6cc9bbb13275a9f5cc237f94e855f5851ff45
FioZow <domahin1995@gmail.com>
2022-07-01 10:45:36 +0300

    Создан каркас экзамена

---
+++ Exam2 (june)/TestF/Program.cs
@@ -0,0 +1,9 @@
+﻿/ var testsCount = int.Parse(Console.ReadLine());
+
+while (testsCount != 0)
+{
+    var a = Console.ReadLine().Split(" ");
+    var b = int.Parse(Console.ReadLine());
+
+    testsCount--;
+}
